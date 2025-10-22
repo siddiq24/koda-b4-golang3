311f649841b3f32042fc8a1327a5ed58bc462ef2
siddiq24 <siddiqrm24@gmail.com>
2025-10-22 11:19:20 +0700

    feat: implement defer, panic, and recovery

--- utils/searchPerson.go
+++ utils/searchPerson.go
@@ -1,12 +1,28 @@
 package utils
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 )
 
+func SearchPerson(list []string, nama *string) (result []string) {
+	defer func() {
+		if r := recover(); r != nil {
+			err := fmt.Sprintln(r)
+			result = []string{err}
+		}
+	}()
 
-func SearchPerson(list []string, nama *string) []string{
-	for _,name := range list{
+	_, err := strconv.Atoi(*nama)
+
+	if nama == nil {
+		panic("insert can not empty")
+	} else if err == nil {
+		panic("insert can not a numerical")
+	}
+
+	for _, name := range list {
 		nama0 := strings.ToLower(name)
 		nama1 := strings.ToLower(*nama)
 		if nama0 == nama1 {
